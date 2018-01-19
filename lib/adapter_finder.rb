0d453436ea14d6dc16fc680313c606f378659e5b
Oli Peate <oli@thoughtbot.com>
2018-01-19 12:09:24 +0000

    Rename ServiceParser to AdapterFinder

--- lib/adapter_finder.rb
+++ lib/adapter_finder.rb
@@ -1,12 +1,12 @@
 require_relative "adapters"
 
-class ServiceParser
+class AdapterFinder
   def self.adapter_for(raw_url)
     matched_adapter = all_adapters.detect do |adapter|
       adapter.match?(raw_url)
     end
 
-    matched_adapter || raise_unrecognised_url(raw_url)
+    matched_adapter || raise_no_adapter(raw_url)
   end
 
   def self.all_adapters
@@ -15,13 +15,16 @@ class ServiceParser
       select { |const| const.respond_to?(:match?) }
   end
 
-  def self.raise_unrecognised_url(raw_url)
-    raise UnrecognisedServiceError.new(raw_url)
+  def self.raise_no_adapter(raw_url)
+    raise NoAdapterFound.new(raw_url)
   end
 
-  class UnrecognisedServiceError < StandardError
+  class NoAdapterFound < StandardError
+    attr_reader :raw_url
+
     def initialize(raw_url)
-      super "Unrecognised service '#{raw_url}'"
+      @raw_url = raw_url
+      super "No adapter available for '#{raw_url}'"
     end
   end
 end
--- spec/adapter_finder_spec.rb
+++ spec/adapter_finder_spec.rb
@@ -1,6 +1,6 @@
-require "service_parser"
+require "adapter_finder"
 
-RSpec.describe ServiceParser do
+RSpec.describe AdapterFinder do
   describe "#adapter_for" do
     it "returns the first matching adapter" do
       expect(described_class.adapter_for("https://flickr.com/photo/1")).
@@ -12,10 +12,11 @@ RSpec.describe ServiceParser do
 
     it "raises an exception for unrecognised URLs" do
       unknown_url = "https://unknown.example"
-      expected_exception = ServiceParser::UnrecognisedServiceError
+      expected_exception = AdapterFinder::NoAdapterFound
+      expected_message = "No adapter available for '#{unknown_url}'"
 
       expect { described_class.adapter_for(unknown_url) }.
-        to raise_exception expected_exception, "Unrecognised service '#{unknown_url}'"
+        to raise_exception expected_exception, expected_message
     end
   end
 end
